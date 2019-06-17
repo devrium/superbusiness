1e11837fbfbe0f3d221807cdc4f4b172c5c495f1
Emmanuel MOZAR <e.mozar@comptoirs.co>
2019-06-17 16:56:56 +0200

    EnableSassLoader

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,6 @@ require('../css/app.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
-require('popper.js');
 require('bootstrap');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
